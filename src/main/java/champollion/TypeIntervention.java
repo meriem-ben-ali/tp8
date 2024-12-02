5fafc7174f58a26324771f91cc74f5a2b325bf2e
meriem-ben-ali <129120484+meriem-ben-ali@users.noreply.github.com>
2024-12-02 11:29:42 +0100

    modif

--- src/main/java/champollion/TypeIntervention.java
+++ src/main/java/champollion/TypeIntervention.java
@@ -4,10 +4,5 @@ package champollion;
  * Les types d'intervention possibles
  */
 public enum TypeIntervention {
-<<<<<<< HEAD
 	CM, TD, TP;
 }
-=======
-	CM, TD, TP;	
-}
->>>>>>> 1ba6895755d7385f2df81218854cf81599022562
